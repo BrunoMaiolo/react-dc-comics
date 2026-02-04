71558a56fc851db93a22ba499ada93dafbded94f
BrunoMaiolo <brunosj@hotmail.it>
2026-02-04 16:41:32 +0100

    aggiunto il bluestrip con lo stile

--- src/components/BlueStrip.jsx
+++ src/components/BlueStrip.jsx
@@ -1,7 +1,34 @@
 function BlueStrip() {
   return (
-    <section>
-      <p>Blue strip</p>
+    <section className="blue-strip">
+      <div className="container blue-strip-items">
+        
+        <div className="blue-item">
+          <img src="/src/assets/img/buy-comics-digital-comics.png" alt="Digital Comics" />
+          <span>Digital Comics</span>
+        </div>
+
+        <div className="blue-item">
+          <img src="/src/assets/img/buy-comics-merchandise.png" alt="DC Merchandise" />
+          <span>DC Merchandise</span>
+        </div>
+
+        <div className="blue-item">
+          <img src="/src/assets/img/buy-comics-subscriptions.png" alt="Subscription" />
+          <span>Subscription</span>
+        </div>
+
+        <div className="blue-item">
+          <img src="/src/assets/img/buy-comics-shop-locator.png" alt="Comic Shop Locator" />
+          <span>Comic Shop Locator</span>
+        </div>
+
+        <div className="blue-item">
+          <img src="/src/assets/img/buy-dc-power-visa.svg" alt="DC Power Visa" />
+          <span>DC Power Visa</span>
+        </div>
+
+      </div>
     </section>
   );
 }
