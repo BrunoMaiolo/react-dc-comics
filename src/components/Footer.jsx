73721a5e8c8f68f21284766d7ec0462bbb6e12bb
BrunoMaiolo <brunosj@hotmail.it>
2026-02-04 16:45:08 +0100

    strutture del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,70 @@
 function Footer() {
   return (
     <footer>
-      <p>Footer</p>
+
+      <div className="footer-top">
+        <div className="container">
+          <div className="footer-columns">
+            <div>
+              <h4>DC COMICS</h4>
+              <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+              </ul>
+
+              <h4>SHOP</h4>
+              <ul>
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+              </ul>
+            </div>
+
+            <div>
+              <h4>DC</h4>
+              <ul>
+                <li>Terms Of Use</li>
+                <li>Privacy policy</li>
+                <li>Ad Choices</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Subscriptions</li>
+                <li>Talent Workshops</li>
+                <li>CPSC Certificates</li>
+                <li>Ratings</li>
+                <li>Shop Help</li>
+                <li>Contact Us</li>
+              </ul>
+            </div>
+
+            <div>
+              <h4>SITES</h4>
+              <ul>
+                <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>DC Kids</li>
+                <li>DC Universe</li>
+                <li>DC Power Visa</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <div className="container footer-bottom-content">
+          <button>SIGN-UP NOW!</button>
+
+          <div className="social">
+            <span>FOLLOW US</span>
+          </div>
+        </div>
+      </div>
+
     </footer>
   );
 }
