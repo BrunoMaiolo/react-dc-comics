ce2076cd17f9cad97ff5f5eee21211c074fdb323
BrunoMaiolo <brunosj@hotmail.it>
2026-02-05 15:26:15 +0100

    sostitutita lista manuale con lista generata dinamicamente

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import navLinks from "../assets/navLinks";
+
 function Header() {
   return (
     <header className="header">
@@ -6,16 +8,9 @@ function Header() {
       
       <nav>
         <ul>
-          <li>Characters</li>
-          <li>Comics</li>
-          <li>Movies</li>
-          <li>TV</li>
-          <li>Games</li>
-          <li>Collectibles</li>
-          <li>Videos</li>
-          <li>Fans</li>
-          <li>News</li>
-          <li>Shop</li>
+          {navLinks.map((link, index) => (
+          <li key={index}>{link}</li>
+           ))}
         </ul>
       </nav>
       </div>
