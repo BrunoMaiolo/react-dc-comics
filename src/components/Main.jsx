08299cea3764678e61db4cf21c046d2cbdc2af63
BrunoMaiolo <brunosj@hotmail.it>
2026-02-05 14:53:07 +0100

    stampa dei fumetti con MAP

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,23 @@
+import comics from "../assets/comics";
+
 function Main() {
   return (
     <main>
-      <div className="container">
-        --&gt; Content goes here &lt;--
-      </div>
+      <section className="comics">
+        <div className="container comics-grid">
+
+          {comics.map((comic, index) => (
+            <div key={index} className="comic-card">
+
+              <img src={comic.thumb} alt={comic.series} />
+
+              <h4>{comic.series}</h4>
+
+            </div>
+          ))}
+
+        </div>
+      </section>
     </main>
   );
 }
